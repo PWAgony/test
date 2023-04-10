00359e9ba5b3b3d50ada3368ca434dec5dfce29b
pengwen <1532547123@qq.com>
2023-04-10 21:06:42 +0800

    70

--- src/concurrent/Test.java
+++ src/concurrent/Test.java
@@ -9,7 +9,7 @@ import java.util.concurrent.*;
  */
 public class Test {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        ThreadPoolExecutor executor = new ThreadPoolExecutor(10, 10,
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(70, 70,
                 0L, TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>());
         CompletableFuture<Integer> future1 = CompletableFuture.supplyAsync(() -> {
             System.out.println("任务1线程：" + Thread.currentThread().getId());
